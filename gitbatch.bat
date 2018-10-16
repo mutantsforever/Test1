aaabc38e5aa47fd5c408ca6e3b45dcd1e964183a
snawasth <sneha.awasthi@capgemini.com>
2018-10-16 15:01:25 +0530

    New Edit

--- gitbatch.bat
+++ gitbatch.bat
@@ -1,5 +1,8 @@
+@echo off
 call git add .
 set /p id="Enter Commit Description: "\r\n
 call git commit -m "%id%"
-call git push https://github.com/grimmhuntress/Test1.git
+echo Wait for Process to complete.....
+REM call git push https://github.com/grimmhuntress/Test1.git
+call git push origin master
 pause
